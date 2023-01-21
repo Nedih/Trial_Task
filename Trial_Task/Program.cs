74a6904966b56b2fc79664a641b45106f25bc082
Nedih <ihor.nedaiev@nure.ua>
2023-01-21 19:30:53 +0200

    Web-site crawler added

--- Trial_Task/Program.cs
+++ Trial_Task/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using cloudscribe.HtmlAgilityPack;
+
+ExtractHref("https://google.com");
+static void ExtractHref(string URL)
+{
+    List<string> sitemapLinks = new List<string>();
+    HtmlWeb web = new HtmlWeb();
+    HtmlDocument doc = new HtmlDocument();
+    doc = web.Load(URL);
+
+    foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+    {
+        HtmlAttribute att = link.Attributes["href"];
+
+
+        //Console.WriteLine(att.Value);
+        string item = att.Value[0] == '/' ? (URL + att.Value) : att.Value;
+        if (att.Value.Contains("/") && !sitemapLinks.Contains(item))
+        {
+            sitemapLinks.Add(item);
+            Console.WriteLine(item);
+        }
+    }
+}
